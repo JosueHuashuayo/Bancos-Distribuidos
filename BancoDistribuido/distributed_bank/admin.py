e69619a3450b14d46adbfaf0a58a4bcd5c80ec25
Josue Huashuayo <64034057+JosueHuashuayo@users.noreply.github.com>
2024-01-02 22:19:29 -0500

    Delete BancoDistribuido directory

--- BancoDistribuido/distributed_bank/admin.py
+++
@@ -1,9 +0,0 @@
-# mi_app/admin.py
-
-from django.contrib import admin
-from .models import Banco, Cliente, Cuenta, Transaccion
-
-admin.site.register(Banco)
-admin.site.register(Cliente)
-admin.site.register(Cuenta)
-admin.site.register(Transaccion)
